ff1c2138713229bf12d019b3ed8efe7864329a79
tmhglnd <timohoogland@gmail.com>
2021-12-20 23:36:35 +0100

    fix speed reverse bug

--- src/core/MonoSample.js
+++ src/core/MonoSample.js
@@ -105,14 +105,14 @@ class MonoSample {
 						this._fx[f].set(c, time, this._bpm);
 					}
 				}
-
+				
 				// get the sample from array
 				let f = Util.getParam(this._sound, c);
 				if (this._bufs.has(f)){
-					this.sample.buffer = this._bufs.get(f);
+					this.sample.buffer = this._bufs.get(f).slice(0);
 				} else {
 					// default sample if file does not exist
-					this.sample.buffer = this._bufs.get('kick_min');
+					this.sample.buffer = this._bufs.get('kick_min').slice(0);
 				}
 				// the duration of the buffer in seconds
 				let dur = this.sample.buffer.duration;
@@ -123,13 +123,7 @@ class MonoSample {
 				// reversing seems to reverse every time the 
 				// value is set to true (so after 2 times reverse
 				// it becomes normal playback again) no fix yet
-				// this.sample.reverse = true;
-				// if (this._rev !== (s < 0)){
-				// 	this.sample.reverse = true;
-				// } else {
-				// 	this.sample.reverse = false;
-				// }
-				// this._rev = (s < 0);
+				this.sample.reverse = s < 0.0;
 
 				let l = Util.lookup(this._stretch, c);
 				let n = 1;
