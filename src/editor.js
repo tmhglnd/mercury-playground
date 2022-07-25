20bfb2198da8cb685c2ed30cc01af6056917d082
tmhglnd <timohoogland@gmail.com>
2022-07-25 14:35:52 +0200

    autocomplete try

--- src/editor.js
+++ src/editor.js
@@ -13,6 +13,8 @@ const saver = require('file-saver');
 require('codemirror/mode/javascript/javascript.js');
 require('codemirror/addon/mode/simple.js');
 require('codemirror/addon/comment/comment.js');
+require('codemirror/addon/hint/show-hint.js');
+require('codemirror/addon/hint/anyword-hint.js');
 
 const defaultTheme = 'material-darker';
 
@@ -60,6 +62,41 @@ CodeMirror.defineSimpleMode("mercury", {
 	]
 });
 
+// CodeMirror.commands.autocomplete = function(cm) {
+// 	cm.showHint({hint: CodeMirror.hint.anyword});
+// }
+
+var WORD = /[\w$]+/, RANGE = 500;
+
+CodeMirror.registerHelper("hint", "anyword", function(editor, options) {
+
+	var word = options && options.word || WORD;
+	var range = options && options.range || RANGE;
+	var cur = editor.getCursor(), curLine = editor.getLine(cur.line);
+	var end = cur.ch, start = end;
+	
+	// while (start && word.test(curLine.charAt(start - 1))) --start;
+	// 	var curWord = start != end && curLine.slice(start, end);
+
+	// 	var list = options && options.list || [], seen = {};
+	// 	var re = new RegExp(word.source, "g");
+	// 	for (var dir = -1; dir <= 1; dir += 2) {
+	// 		var line = cur.line, endLine = Math.min(Math.max(line + dir * range, editor.firstLine()), editor.lastLine()) + dir;
+	// 		for (; line != endLine; line += dir) {
+	// 			var text = editor.getLine(line), m;
+	// 			while (m = re.exec(text)) {
+	// 				if (line == cur.line && m[0] === curWord) continue;
+	// 				if ((!curWord || m[0].lastIndexOf(curWord, 0) == 0) && !Object.prototype.hasOwnProperty.call(seen, m[0])) {
+	// 				seen[m[0]] = true;
+	// 				list.push(m[0]);
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	list = ['new', 'set', 'synth'];
+	return {list: list, from: CodeMirror.Pos(cur.line, start), to: CodeMirror.Pos(cur.line, end)};
+});
+
 const Editor = function({ context, engine, canvas }) {
 	// this._engine = engine;
 	console.log('=> Created Editor()');
@@ -94,12 +131,20 @@ const Editor = function({ context, engine, canvas }) {
 			'Alt-Enter': () => { this.evaluate() },
 			'Alt-.': () => { this.silence() },
 			'Tab': 'insertSoftTab',
-		}
+			'Ctrl-,': 'autocomplete',
+		},
 	}
 
 	this.cm = CodeMirror.fromTextArea(text, this.options);
 
 	this.cm.markText({line: 0, ch: 0}, {line: 6, ch: 42}, {className: 'styled-background'})
+
+	this.cm.on('change', () => { 
+		//console.log('hi')
+		// this.cm.execCommand('autocomplete');
+		// this.cm.showHint();
+		setTimeout(() => { this.cm.execCommand('autocomplete'), 10 }); 
+	});
 
 	this.set = async function(v){
 		this.cm.setValue(v);
