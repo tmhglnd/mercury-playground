0217cddceee7cb67ab711cf9714ab0ebfda67361
tmhglnd <timohoogland@gmail.com>
2023-05-16 21:51:09 +0200

    ported squash/drive to audioworkletprocessor

--- src/core/effects/Processors.js
+++ src/core/effects/Processors.js
@@ -15,8 +15,9 @@ class NoiseProcessor extends AudioWorkletProcessor {
 }
 registerProcessor('noise-processor', NoiseProcessor);
 
-// A downsampling/degrading processing effect
-// Sample and holds the signal based on an incrementing counter
+// A Downsampling Chiptune effect. Downsamples the signal by a specified amount
+// Resulting in a lower samplerate, making it sound more like 8bit/chiptune
+// Programmed with a custom AudioWorkletProcessor, see effects/Processors.js
 //
 class DownSampleProcessor extends AudioWorkletProcessor {
 	static get parameterDescriptors() {
@@ -24,7 +25,7 @@ class DownSampleProcessor extends AudioWorkletProcessor {
 			name: 'down',
 			defaultValue: 8,
 			minValue: 1,
-			maxValue: 1024
+			maxValue: 2048
 		}];
 	}
 
@@ -44,10 +45,11 @@ class DownSampleProcessor extends AudioWorkletProcessor {
 		if (input.length > 0){
 			// for the length of the sample array (generally 128)
 			for (let i=0; i<input[0].length; i++){
+				const d = (parameters.down.length > 1) ? parameters.down[i] : parameters.down[0];
 				// for every channel
 				for (let channel=0; channel<input.length; ++channel){
 					// if counter equals 0, sample and hold
-					if (this.count % parameters.down === 0){
+					if (this.count % d === 0){
 						this.sah[channel] = input[channel][i];
 					}
 					// output the currently held sample
@@ -61,3 +63,68 @@ class DownSampleProcessor extends AudioWorkletProcessor {
 	}
 }
 registerProcessor('downsampler-processor', DownSampleProcessor);
+
+// A distortion algorithm using the tanh (hyperbolic-tangent) as a 
+// waveshaping technique. Some mapping to apply a more equal loudness 
+// distortion is applied on the overdrive parameter
+//
+class TanhDistortionProcessor extends AudioWorkletProcessor {
+	constructor(){
+		super();
+	}
+
+	process(inputs, outputs, parameters){
+		const input = inputs[0];
+		const output = outputs[0];
+
+		if (input.length > 0){
+			for (let channel=0; channel<input.length; ++channel){
+				for (let i=0; i<input[channel].length; i++){
+					// simple waveshaping with tanh
+					output[channel][i] = Math.tanh(input[channel][i]);
+				}
+			}
+		}
+		return true;
+	}
+}
+registerProcessor('tanh-distortion-processor', TanhDistortionProcessor);
+
+// A distortion/compression effect of an incoming signal
+// Based on an algorithm by Peter McCulloch
+// 
+class SquashProcessor extends AudioWorkletProcessor {
+	static get parameterDescriptors(){
+		return [{
+			name: 'amount',
+			defaultValue: 4,
+			minValue: 1,
+			maxValue: 1024
+		}];
+	}
+
+	constructor(){
+		super();
+	}
+
+	process(inputs, outputs, parameters){
+		const input = inputs[0];
+		const output = outputs[0];
+		
+		if (input.length > 0){
+			for (let channel=0; channel<input.length; ++channel){
+				for (let i=0; i<input[channel].length; i++){
+					// (s * a) / ((s * a)^2 * 0.28 + 1) / √a
+					// drive amount, minimum of 1
+					const a = (parameters.amount.length > 1)? parameters.amount[i] : parameters.amount[0];
+					// set the waveshaper effect
+					const s = input[channel][i];
+					const p = (s * a) / ((s * a) * (s * a) * 0.28 + 1.0);
+					output[channel][i] = p;
+				}
+			}
+		}
+		return true;
+	}
+}
+registerProcessor('squash-processor', SquashProcessor);
