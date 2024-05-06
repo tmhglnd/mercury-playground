fe5796df627db5a479337f1f6375e6e914498d03
tmhglnd <timohoogland@gmail.com>
2024-05-06 17:47:40 +0200

    new reverb based on freeverb and jcrev implemented

--- src/core/Effects.js
+++ src/core/Effects.js
@@ -49,7 +49,7 @@ const fxMap = {
 	'reverb' : (params) => {
 		return new Reverb(params);
 	},
-	'freeverb' : (params) => {
+	'tverb' : (params) => {
 		return new CustomFreeverb(params);
 	},
 	'shift' : (params) => {
@@ -406,19 +406,28 @@ const AllPass = function(dt=10, res=0.5){
 // less expensive, more in the range of the JCRev
 // 
 const CustomFreeverb = function(_params){
-	this.nodes = [];
+	_params = Util.mapDefaults(_params, [ 0.5, 1, 1, 0.5 ]);
+	_params = _params.map((p) => Util.toArray(p));
+	this._wet = _params[0];
+	this._size = _params[1];
+	this._decay = _params[2];
+	this._damp = _params[3];
 
+	// initial delaytimes and resonance values (also factor for multiply)
 	this._apIn = [ 0.01388, 0.00452, 0.00148 ];
 	this._apOut = [ 0.01261, 0.00773 ];
-	this._apQ = 0.523;
+	this._apQ = 0.423;
 
 	this._delays = [ 0.035306, 0.028957, 0.03667, 0.030749, 
 					0.03381, 0.026938, 0.032245, 0.025306 ];
-	this._fb = 0.97 //0.84;
+	this._fb = 0.84;
 
-	// the input allpass diffusion section plus onepole lowpass filter
-	this._lpf = new Tone.OnePoleFilter(2000, "lowpass");
+	// the wet/dry mix and output
+	this._mix = new Tone.Add();
+	this._mixDry = new Tone.Gain(0.5).connect(this._mix.addend);
 
+	// the input allpass diffusion section plus onepole lowpass filter for damp
+	this._lpf = new Tone.OnePoleFilter(500, "lowpass");
 	this._ap3 = new AllPass(this._apIn[2], this._apQ);
 	this._ap3.connect(this._lpf);
 	this._ap2 = new AllPass(this._apIn[1], this._apQ);
@@ -426,13 +435,19 @@ const CustomFreeverb = function(_params){
 	this._ap1 = new AllPass(this._apIn[0], this._apQ);
 	this._ap1.connect(this._ap2.in);
 
-	// the input node
-	this._fx = new Tone.Gain(1).connect(this._ap1.in);
+	// the input node for Dry and Wet split
+	this._fx = new Tone.Gain(1).fan(this._ap1.in, this._mixDry);
 	
+	// the channel merger for stereo output
+	this._out = new Tone.Merge().connect(this._mix);
+
 	// the various outputs for stereo image/mixing
 	this._outA = new Tone.Gain(1);
-	this._outB = new Tone.Subtract();
-	this._outC = new Tone.Subtract();
+	this._outA.connect(this._out, 0, 0).connect(this._out, 0, 1); // use both LR
+	this._outB = new Tone.Subtract().connect(this._out, 0, 0); // use L
+	this._outB.output.gain.setValueAtTime(0.2, time); // reduce the side volume
+	this._outC = new Tone.Subtract().connect(this._out, 0, 1); // use R
+	this._outC.output.gain.setValueAtTime(0.2, time); // reduce the side volume
 
 	// the output outA diffusion section 
 	this._ap5 = new AllPass(this._apOut[1], this._apQ);
@@ -446,29 +461,51 @@ const CustomFreeverb = function(_params){
 	this._s2 = new Tone.Gain(0.2);
 	this._s2.fan(this._ap4.in, this._outB.subtrahend, this._outC);
 
+	// create 8 combfilters and split between s1 and s2 for summing
 	this._combs = [];
 	for (var i=0; i<this._delays.length; i++){
 		let comb = new Tone.FeedbackCombFilter(this._delays[i], this._fb);
+		// comb.output.gain.setValueAtTime(0.2, time);
 		comb.connect((i % 2) ? this._s1 : this._s2);
-		
-		this._ap3.connect(comb);
+		this._lpf.connect(comb);
 		this._combs.push(comb);
 	}
 
+	// set the parameters based on the arguments
 	this.set = function(c, time){
+		let wet = Util.getParam(this._wet, c);
+		let size = Util.getParam(this._size, c);
+		let decay = Util.getParam(this._decay, c);
+		let damp = Util.getParam(this._damp, c);
 
+		// equal power crossfade midpoint -3dB(~0.707) for uncorrelated signals
+		let dry = Math.cos(wet * 1.5707);
+		this._mixDry.gain.setValueAtTime(dry, time);
+		
+		wet = Math.cos(wet * 1.5708 + 4.7124);
+		this._s1.gain.setValueAtTime(wet, time);
+		this._s2.gain.setValueAtTime(wet, time);
+
+		this._lpf.frequency = Util.clip(damp * damp * damp) * 9900 + 100;
+
+		for (var i=0; i<this._combs.length; i++){
+			let dt = (Util.clip(size) * 1.8 + 0.5) * this._delays[i];
+			let fb = (Util.clip(decay ** 0.5) * 0.37 + 0.8) * this._fb;
+			this._combs[i].delayTime.setValueAtTime(dt, time);
+			this._combs[i].resonance.setValueAtTime(fb, time);
+		}
 	}
 
 	this.chain = function(){
-		return { 'send' : this._fx, 'return' : this._outA }
+		return { 'send' : this._fx, 'return' : this._mix }
 	}
 
 	this.delete = function(){
-		let nodes = [ this._lpf, this._ap5, this._ap4, this._ap3, this._ap2, this._ap1, this._fx, this._outA, this._outB, this._outC, this._s1, this._s2, ...this._combs ];
+		let nodes = [ this._lpf, this._ap5, this._ap4, this._ap3, this._ap2, this._ap1, this._fx, this._outA, this._outB, this._outC, this._s1, this._s2, ...this._combs, this._out, this._mix, this._mixDry ];
 
 		nodes.forEach((n) => { 
-			n.disconnect(); 
-			n.dispose(); 
+			n?.disconnect(); 
+			n?.dispose(); 
 		});
 	}
 }
