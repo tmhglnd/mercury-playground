e25ebe34ffd497a4eae7550a20bb0bb4c959b75e
tmhglnd <timohoogland@gmail.com>
2026-05-13 14:38:22 +0200

    another try at pwa

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,40 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+// import { staticResourceCache } from 'workbox-recipes';
+// if (workbox) {
+// 	console.log('[workbox] loaded successfully');
+// } else {
+// 	console.log('[workbox] failed to load');
+// }
 
-import { staticResourceCache } from 'workbox-recipes';
+const cacheName = 'cache-mercury';
+const precacheResources = [
+	'/',
+	'index.html',
+	'bundle.js',
+	'style.css'
+]
 
-if (workbox) {
-	console.log('[workbox] loaded successfully');
-} else {
-	console.log('[workbox] failed to load');
-}
+self.addEventListener('install', (event) => {
+	console.log('[serviceworker] install');
+	event.waitUntil(
+		caches.open(cacheName)
+		.then(cache => {
+			return cache.addAll(precacheResources);
+		})
+	)
+});
+
+// self.addEventListener('activate', (event) => {
+
+// });
+
+self.addEventListener('fetch', (event) => {
+	// event.request.url
+	event.respondWith(
+		caches.match(event.request)
+		.then(cachedResponse => {
+			return cachedResponse || fetch(event.request)
+		})
+	);
+});
 
